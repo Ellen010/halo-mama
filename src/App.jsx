74eecbbee2e939b45055a69e3546781ba8df8521
Elena <157579304+Ellen010@users.noreply.github.com>
2025-10-27 14:16:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import gsap from 'gsap';
+import {ScrollTrigger, SplitText} from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
     return (
         <div className="text-red-800">App</div>
