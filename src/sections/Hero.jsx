ce02e490811bdb275c5a8bc643a95ba4c30c8a29
Ellen010 <elenawelty@gmail.com>
2025-12-02 19:28:04 +0100

    hero addition corrections

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
     <div className="flex items-center flex-col gap-4">
         <img src="/product.png" alt="Halo-mama product" className="product" />
-       <h1 className="flex-center mx-20 my-10 text-3xl titan-one-regular leading-loose">Here’s a short, warm welcoming message you can use on your website:
+       <h1 className="mx-20 my-10 text-3xl titan-one-regular leading-loose">Here’s a short, warm welcoming message you can use on your website:
         Welcome to our platform — a space created by a mom-to-be, for moms-to-be.
            During her own pregnancy, our founder felt the deep need for a pillow that truly
            supports a woman’s changing body, and that’s how this revolutionary pregnancy
