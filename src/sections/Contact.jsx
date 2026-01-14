4bbecf57624c90699f0420b8a3fb898decd7f67b
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-01-14 19:00:57 +0100

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,79 +1,101 @@
 import React from 'react'
+import emailjs from '@emailjs/browser';
 import { useRef, useState } from 'react';
-import useAlert from '../hooks/useAlert.jsx';
+import useAlert from '../hooks/useAlert.jsx'
 import Alert from '../components/Alert.jsx'
+import TripleLogo from '/public/images/big-logo.png'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
+import { animateWithGsap } from '../assets/constants/animations.js';
 
-
-const formRef = useRef();
-
-const { alert, showAlert, hideAlert } = useAlert();
-const [loading, setLoading] = useState(false);
-
-const [form, setForm] = useState({ name: '', email: '', message: '' });
-
-const handleChange = ({ target: { name, value } }) => {
-    setForm({ ...form, [name]: value });
-};
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs.send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-            from_name: form.name,
-            from_email: form.email,
-            message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-    )
-        .then(
-            () => {
-                setLoading(false);
-                showAlert({
-                    show: true,
-                    text: 'Votre message a bien été envoyé. Nous vous répondrons dans les plus brefs délais',
-                    type: 'success',
-                });
-
-                setTimeout(() => {
-                    hideAlert(false);
-                    setForm({
-                        name: '',
-                        email: '',
-                        message: '',
-                    });
-                }, [6000]);
-            },
-            (error) => {
-                setLoading(false);
-                console.error(error);
-
-                showAlert({
-                    show: true,
-                    text: "Error occurred and the message hasn't been sent",
-                    type: 'danger',
-                });
-            },
-        );
-};
 const Contact = () => {
+    useGSAP(() => {
+        gsap.from('#contact', {
+            scrollTrigger: {
+                trigger: '#contact',
+                start: '20% bottom'
+            },
+            opacity: 0,
+            scale: 2,
+            duration: 2,
+            ease: 'power2.inOut'
+        })
+
+        animateWithGsap('.g_fadeIn', {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: 'power2.inOut'
+        })
+    }, []);
+    const formRef = useRef();
+
+    const { alert, showAlert, hideAlert } = useAlert();
+    const [loading, setLoading] = useState(false);
+
+    const [form, setForm] = useState({ name: '', email: '', message: '' });
+
+    const handleChange = ({ target: { name, value } }) => {
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        emailjs.send(
+            import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+            import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+            {
+                from_name: form.name,
+                from_email: form.email,
+                message: form.message,
+            },
+            import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        )
+            .then(
+                () => {
+                    setLoading(false);
+                    showAlert({
+                        show: true,
+                        text: 'Your message has been sent successfully. We will reply as soon as possible.',
+                        type: 'success',
+                    });
+
+                    setTimeout(() => {
+                        hideAlert(false);
+                        setForm({
+                            name: '',
+                            email: '',
+                            message: '',
+                        });
+                    }, [6000]);
+                },
+                (error) => {
+                    setLoading(false);
+                    console.error(error);
+
+                    showAlert({
+                        show: true,
+                        text: "Error occurred and the message hasn't been sent",
+                        type: 'danger',
+                    });
+                },
+            );
+    };
+
     return (
         <section id="contact">
 
-            <h1 className="title g_fadeIn">Contact</h1>
+            <h1 className="title g_fadeIn momo-signature-regular text-5xl font-extrabold">Contact</h1>
             <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
                 <div className="photo-left g_fadeIn">
-                    <img src="/contact-pic.png" alt="ÉlanFinance Employe working on a financial report" />
+                    <img src={TripleLogo} alt="Halo-mama triple logo" />
                 </div>
 
 
                 <div className="w-full text-xl lg:w-1/2 ms-5 ml-30 text-justify lg:text-left leading-relaxed">
-                    <p className="mini-title g_fadeIn">Contactez-nous pour plus d'informations </p>
+                    <p className="mini-title g_fadeIn">Contact us today</p>
                     <div className="contact-container">
 
                         {alert.show && <Alert {...alert} />}
