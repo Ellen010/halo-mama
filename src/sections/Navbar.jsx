391b5ef69856bc364fe6dbd4d427bfcf7eaeb092
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-02-06 19:30:51 -0500

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -8,16 +8,17 @@ const Navbar = () => {
 
     return (
         <header className="w-full">
-            <nav className="relative flex items-center justify-between w-full px-6 md:px-12 py-4">
+            <nav className="relative flex items-center justify-between w-full py-4">
 
-                <div className="flex items-center gap-4 z-20">
-                    <img src="/images/logo.png" alt="Halo-mama logo" className="rounded-[20%] h-auto w-[60px] sm:w-[80px] md:w-[110px] lg:w-[150px] xl:w-[180px] transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-[1em_1em_1em_#a1a2ad] " />
-                    <h1 className="momo-signature-regular absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[6vw] md:text-base lg:text-lg xl:text-lg 2xl:text-5xl 3xl:text-6xl text-[--color-primary] font-bold title-hover-shadow">
+                <div className="flex items-center gap-3 z-20">
+                    <img src="/images/logo.png" alt="Halo-mama logo" className="rounded-[20%] h-auto w-[60px] sm:w-[80px] md:w-[110px] lg:w-[130px] xl:w-160px] transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-[1em_1em_1em_#a1a2ad] " />
+                    <h1 className="momo-signature-regular absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[8vw] sm:text-[6vw] md:text-[3vw] lg:text-3xl xl:text-4xl 2xl:text-6xl text-[--color-primary] font-bold title-hover-shadow leading-none whitespace-nowrap title-shadow">
                         Halo-mama
                     </h1>
+
                 </div>
 
-                <ul className="hidden xl:flex items-center gap-10 momo-signature-regular z-20">
+                <ul className="hidden xl:flex items-center gap-4 momo-signature-regular z-20 text-[2vw] sm:text-[4vw] md:text-[3vw] lg:text-lg xl:text-xl 2xl:text-2xl text-[--color-primary]">
                     {navLinks.map((link) => (
                         <li key={link.label}>
                             <a href={link.href} className="menu">
@@ -33,6 +34,7 @@ const Navbar = () => {
                 >
                     {open ? <X size={36} /> : <Menu size={36} />}
                 </button>
+
             </nav>
 
             {open && (
